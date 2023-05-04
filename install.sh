3f7ee9de1e5dc883b9541e9140689676a749aaba
dhanya2010402 <96098456+dhanya2010402@users.noreply.github.com>
2023-05-04 22:44:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-//
+#!/bin/bash
+
+conda create --name SSNdhanyadivyakavitha_taskA_venv python=3.8
+conda activate SSNdhanyadivyakavitha_taskA_venv
+conda install pandas scikit-learn imbalanced-learn nltk
+conda deactivate
+
