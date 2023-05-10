ba25c69650bfc2cb55478469aac25d8d0e28348f
dhanya2010402 <96098456+dhanya2010402@users.noreply.github.com>
2023-05-10 20:20:38 +0530

    Update decode_taskA_run1.sh

--- decode_taskA_run1.sh
+++ decode_taskA_run1.sh
@@ -1,50 +1,19 @@
-import argparse
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import Pipeline
-from sklearn.svm import LinearSVC
-from sklearn.metrics import accuracy_score
-from imblearn.over_sampling import RandomOverSampler
+#!/bin/bash
 
-import string
-import re
-import nltk
-from nltk.corpus import stopwords
+# Check if a CSV file path is provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 [CSV_FILE_PATH]"
+  exit 1
+fi
 
-nltk.download('stopwords')
-stop_words = stopwords.words('english')
+# Get the CSV file path from the command line argument
+csv_file=$1
 
-def preprocess_text(text):
-    # Remove punctuation
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    # Remove digits
-    text = re.sub(r'\d+', '', text)
-    # Convert to lowercase
-    text = text.lower()
-    # Remove stop words
-    text = ' '.join([word for word in text.split() if word not in stop_words])
-    return text
+# Check if the CSV file exists
+if [ ! -f "$csv_file" ]; then
+  echo "Error: File $csv_file not found."
+  exit 1
+fi
 
-def main(test_file):
-    val_df = pd.read_csv(test_file)
-    val_df['dialogue'] = val_df['dialogue'].apply(preprocess_text)
-
-    tfidf = TfidfVectorizer(max_features=10000)
-
-    X_val = tfidf.transform(val_df['dialogue']).toarray()
-    y_val = val_df['section_header']
-
-    svc_model = Pipeline([('svc', LinearSVC())])
-    svc_model.fit(X_train_resampled, y_train_resampled)
-    y_pred = svc_model.predict(X_val)
-
-    # Write the output to a CSV file
-    output_df = pd.DataFrame({'section_header': y_pred})
-    output_df.to_csv('output.csv', index=False)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('test_file', help='Path to the test CSV file')
-    args = parser.parse_args()
-
-    main(args.test_file)
+# Run the Python program
+python run1.py $csv_file
