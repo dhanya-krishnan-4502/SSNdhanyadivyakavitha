b67f6cfb016e401e0b221afd5761124f457b8103
dhanya2010402 <96098456+dhanya2010402@users.noreply.github.com>
2023-05-10 20:19:38 +0530

    Update activate.sh

--- activate.sh
+++ activate.sh
@@ -1,4 +1,16 @@
-
 #!/bin/bash
 
-conda activate SSNdhanyadivyakavitha_taskA_venv
+# Create virtual environment
+python3 -m venv SSNdhanyadivyakavitha_taskA_venv
+
+# Activate virtual environment
+source SSNdhanyadivyakavitha_taskA_venv/bin/activate
+
+# Install required packages
+pip install pandas numpy scikit-learn imbalanced-learn nltk
+
+# install specific versions of scikit-learn and joblib
+pip install -U scikit-learn==0.24.2 joblib==1.0.1
+
+# download NLTK data
+python -m nltk.downloader wordnet
