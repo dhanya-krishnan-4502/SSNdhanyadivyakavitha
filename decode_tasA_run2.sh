143f5a4474c7dcbdeb513a68682f13ff4ba2a7a3
dhanya2010402 <96098456+dhanya2010402@users.noreply.github.com>
2023-05-10 22:51:14 +0530

    Create decode_tasA_run2.sh

---
+++ decode_tasA_run2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if a CSV file path is provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 [CSV_FILE_PATH]"
+  exit 1
+fi
+
+# Get the CSV file path from the command line argument
+csv_file=$1
+
+# Check if the CSV file exists
+if [ ! -f "$csv_file" ]; then
+  echo "Error: File $csv_file not found."
+  exit 1
+fi
+
+# Run the Python program
+python run2.py $csv_file
